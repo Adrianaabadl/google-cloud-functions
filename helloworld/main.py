c0aa1af762c12db33df09d986f7265ce0b58268c
Adrianaabadl <adri.abad.limon@gmail.com>
2020-12-02 00:16:49 -0300

    add one more parameter

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,10 +1,12 @@
 def hello_world(request):
     request_args = request.args
-    if request_args and 'name' in request_args:
+    if request_args and 'name' in request_args and 'lastname' in request_args:
         name = request_args['name']
+        lastname = request_args['lastname']
     else:
         name = 'World'
-    return f'Hello {name}'
+        lastname = ''
+    return f'Hello {name} {lastname}'
 
 
 # if you have already have a process bound to the default port (8000)
